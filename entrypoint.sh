73b82c454471ec616676ac8c0ac9d8b654af0153
Francesco Ciocchetti <fciocchetti@mintel.com>
2020-01-07 11:08:46 +0100

    Add random string to logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,14 +12,15 @@ do
    sleep $(echo "scale=4; $WAIT/1000" | bc)
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
+   RND=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 256 | head -n 1`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled. RND: $RND"
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task. RND: $RND"
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable. RND: $RND"
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored. RND: $RND"
       ;;
    esac
    c=$(( c+1 ))
